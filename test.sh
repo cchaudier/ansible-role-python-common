4b7e949f2e1bcd11087d6e640c33855e77910bd5
Steve Tjoa <kiemyang@gmail.com>
2014-03-29 15:57:54 -0700

    removing unnecessary package installs

--- test.sh
+++ test.sh
@@ -2,14 +2,13 @@
 
 # install pip
 apt-get -qq update
-apt-get -y install python-dev python-pip git
-pip install --upgrade pip
+apt-get -y install python-pip
 
 # taken from .travis.yml
-pwd
-python --version
-/usr/local/bin/pip install ansible
 cd /vagrant
+python --version
+pwd
+/usr/local/bin/pip install ansible
 ansible-playbook -i hosts --connection=local test.yml
 dpkg -s python-dev | grep Status
 pip --version
